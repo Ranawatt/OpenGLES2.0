9700d54148ca0672dc1138196b812b33c6cb2d57
Sugandh Kumar <sugandhpatna95@gmail.com>
2021-04-22 19:55:53 +0530

    Applied projection and camera views

--- app/src/main/java/com/example/opengles20/MyGLRenderer.kt
+++ app/src/main/java/com/example/opengles20/MyGLRenderer.kt
@@ -2,12 +2,19 @@ package com.example.opengles20
 
 import android.opengl.GLES20
 import android.opengl.GLSurfaceView
+import android.opengl.Matrix
 import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
 
 class MyGLRenderer: GLSurfaceView.Renderer {
     private lateinit var mTriangle: Triangle
     private lateinit var mSquare: Square
+
+    // vPMatrix is an abbreviation for "Model View Projection Matrix"
+    private val vPMatrix = FloatArray(16)
+    private val projectionMatrix = FloatArray(16)
+    private val viewMatrix = FloatArray(16)
+
     override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
         // Set the background frame color
         GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f)
@@ -19,12 +26,27 @@ class MyGLRenderer: GLSurfaceView.Renderer {
 
     override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) {
         GLES20.glViewport(0, 0, width, height)
+
+        val ratio: Float = width.toFloat() / height.toFloat()
+
+        // this projection matrix is applied to object coordinates
+        // in the onDrawFrame() method
+        Matrix.frustumM(projectionMatrix, 0, -ratio, ratio, -1f, 1f, 3f, 7f)
     }
 
     override fun onDrawFrame(gl: GL10?) {
         // Redraw background color
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
-        mTriangle.draw()
+//        mTriangle.draw()
+
+        // Set the camera position (View matrix)
+        Matrix.setLookAtM(viewMatrix, 0, 0f, 0f, -3f, 0f, 0f, 0f, 0f, 1.0f, 0.0f)
+
+        // Calculate the projection and view transformation
+        Matrix.multiplyMM(vPMatrix, 0, projectionMatrix, 0, viewMatrix, 0)
+
+        // Draw shape
+        mTriangle.draw(vPMatrix)
     }
 
 }
